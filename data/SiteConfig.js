82f02c3dadd4845a88aa29a6b402d970945aee13
Thomas K. Running <thomas@kjemperud.net>
2019-08-09 16:28:33 +0200

    fix canonical links

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -3,8 +3,8 @@ module.exports = {
   siteTitleShort: "Nomad Gate", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
   siteTitleAlt: "Behind The Scenes", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
-  siteUrl: "https://nomadgate.com", // Domain of your website without pathPrefix.
-  pathPrefix: "/bts", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
+  siteUrl: "https://bts.nomadgate.com", // Domain of your website without pathPrefix.
+  //pathPrefix: "/bts", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
   siteDescription: "A look behind the scenes of Nomad Gate", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
   // siteFBAppID: "1825356251115265", // FB Application ID for using app insights
